a24d51307418ba3768484569ca92d491f5b0c333
David Blanchard <david.blanchard@itelios.com>
2017-04-19 19:04:48 +0200

    Try to autoload js file from data attribute

--- HelloWorld/lib/phink/main.js
+++ HelloWorld/lib/phink/main.js
@@ -1,19 +1,45 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 var Phink = function() {}
-
 Phink.DOM = Phink.DOM || {}
-
 Phink.DOM.ready = function (f){/in/.test(document.readyState)?setTimeout('Phink.DOM.ready('+f+')',9):f()}
 
+var loaded = [];
+
 Phink.include = function (file) {
-    var myScript =  document.createElement("script")
-    myScript.src = file
-    myScript.type = "text/javascript"
-    document.body.appendChild(myScript)
+    loaded[file] = 'false';
+    var myScript =  document.createElement("script");
+    myScript.src = file;
+    myScript.type = "text/javascript";
+    myScript.addEventListener('load', function() {
+        loaded[file] = 'true';
+    })
+    document.body.appendChild(myScript);
 }
+
+var mainNode = document.querySelectorAll("script[src='/phink/main.js']");
+var sources = (mainNode.length > 0 && mainNode[0].dataset['sources'] !== undefined) ? mainNode[0].dataset['sources'].split(";") : [];
+var init = (mainNode.length > 0 && mainNode[0].dataset['init'] !== undefined) ? mainNode[0].dataset['init'].split(";") : [];
+
+Phink.DOM.ready(function () {
+    for (var i = 0; i < sources.length; i++) {
+        Phink.include(sources[i]);
+    }
+
+    for (var i = 0; i < init.length; i++) {
+        //var f = function () { setTimeout(init[i], 10) };
+        console.log(init[i] + ':' + (typeof f));
+        
+        var F  = function (f){
+            if(/true/.test(loaded[f])) {
+                console.log(f + ':' + (typeof f));
+                setTimeout('F('+f+')',10) 
+            } else {
+                console.log(f + ':' + (typeof f));
+                setTimeout(f, 10)
+            };
+        }
+
+        F(init[i]);
+
+    }
+})
+
