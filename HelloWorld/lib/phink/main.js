a8d23b053f77c66749dfb5d0bca1e09063b1d152
David Blanchard <dblanchard1@bbox.fr>
2017-04-22 12:51:38 +0200

    limit initialization with only one function

--- HelloWorld/lib/phink/main.js
+++ HelloWorld/lib/phink/main.js
@@ -3,33 +3,29 @@ Phink.DOM = Phink.DOM || {}
 Phink.DOM.ready = function (f){/in/.test(document.readyState)?setTimeout('Phink.DOM.ready('+f+')',9):f()}
 
 Phink.include = function (file, callback) {
-    var myScript =  document.createElement("script");
-    myScript.src = file;
-    myScript.type = "text/javascript";
+    var tag =  document.createElement("script");
+    tag.src = file;
+    tag.type = "text/javascript";
 
-    myScript.addEventListener('load', function(e) {
+    tag.addEventListener('load', function(e) {
         if(typeof callback === 'function') {
             callback.call(null, e);    
         }
     })
-    document.body.appendChild(myScript);
+    document.body.appendChild(tag);
 }
 
 var mainNode = document.querySelectorAll("script[src='/phink/main.js']");
 var sources = (mainNode.length > 0 && mainNode[0].dataset['sources'] !== undefined) ? mainNode[0].dataset['sources'].split(";") : [];
-var init = (mainNode.length > 0 && mainNode[0].dataset['init'] !== undefined) ? mainNode[0].dataset['init'].split(";") : [];
+var init = (mainNode.length > 0 && mainNode[0].dataset['init'] !== undefined) ? mainNode[0].dataset['init'] : null;
 
 Phink.DOM.ready(function () {
     for (var i = 0; i < sources.length; i++) {
         Phink.include(sources[i], function(e) {
-            for (var i = 0; i < init.length; i++) {
-                if(typeof window[init[i]] === 'function') {
-                    window[init[i]]();
-                    init.pop(init[i]);                  
-                }
+            if(typeof window[init] === 'function') {
+                window[init]();
             }
         });
     }
-    
 });
 
