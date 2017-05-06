1c02eee9535937241a6f491ad2d71bc7f99aad5b
dpjb78 <dpjb78@gmail.com>
2017-05-06 11:45:33 -0400

    try to load javascript before init call

--- HelloWorld/web/js/index.js
+++ HelloWorld/web/js/index.js
@@ -1,68 +1,75 @@
+
 var currentUser = 1;
-var usr = new SoundLib.User(currentUser);
-var pl = new SoundLib.Playlist(currentUser);
-var coll = new SoundLib.Collection();
+var usr = null;
+var pl = null;
+var coll = null;
 
-var getCollection = function() {
-    coll.fetch(function(data) {
-        console.log(data);
-        var result = '<ol>';
-        data = data.collection;
+var getCollection = null;
+var getUserFavorites = null;
 
-        for(var i = 0; i < data.length; i++) {
-            var duration = data[i].duration;
+Phink.DOM.ready(function() {
+  
+    currentUser = 1;
+    usr = new SoundLib.User(currentUser);
+    pl = new SoundLib.Playlist(currentUser);
+    coll = new SoundLib.Collection();
 
-            var minutes = Math.floor(duration / 60);
-            var seconds = duration - (minutes * 60);
-            duration = minutes + ':' + ('00' + seconds).toString().slice(-2);
+    var getCollection = function() {
+        coll.fetch(function(data) {
+            console.log(data);
+            var result = '<ol>';
+            data = data.collection;
 
-            result += '<li><a href="javascript:pl.addTrack(' + data[i].id + ')" ><img src="/css/images/add.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>';
-        }
-        result += '</ol>';
-        
-        var div = document.getElementById('collection');
-        if(div !== undefined) {
-            div.innerHTML = result;
-        }
-    })
-    
-}
-
-var getUserFavorites = function() {
-    pl.getFavorites(function(data) {
-        var result = '<ol>';
-        data = data.playlist;
-        if(data[0].artist === null && data[0].title === null) {
-            result = 'La playlist est vide';
-        } else {
             for(var i = 0; i < data.length; i++) {
                 var duration = data[i].duration;
-                
+
                 var minutes = Math.floor(duration / 60);
                 var seconds = duration - (minutes * 60);
                 duration = minutes + ':' + ('00' + seconds).toString().slice(-2);
-            
-                result += '<li><a href="javascript:pl.removeTrack(' + data[i].id + ')" ><img src="/css/images/delete.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>';
+
+                result += '<li><a href="javascript:pl.addTrack(' + data[i].id + ')" ><img src="/css/images/add.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>';
             }
             result += '</ol>';
-        }
-        
-        var div = document.getElementById('playlist');
-        if(div !== undefined) {
-            div.innerHTML = result;
-        }
-    })
-    
-}
 
-pl.afterAddTrack = getUserFavorites;
-pl.afterRemoveTrack = getUserFavorites;
+            var div = document.getElementById('collection');
+            if(div !== undefined) {
+                div.innerHTML = result;
+            }
+        })
 
+    }
 
+    var getUserFavorites = function() {
+        pl.getFavorites(function(data) {
+            var result = '<ol>';
+            data = data.playlist;
+            if(data[0].artist === null && data[0].title === null) {
+                result = 'La playlist est vide';
+            } else {
+                for(var i = 0; i < data.length; i++) {
+                    var duration = data[i].duration;
 
-Phink.DOM.ready(function() {
-    
+                    var minutes = Math.floor(duration / 60);
+                    var seconds = duration - (minutes * 60);
+                    duration = minutes + ':' + ('00' + seconds).toString().slice(-2);
+
+                    result += '<li><a href="javascript:pl.removeTrack(' + data[i].id + ')" ><img src="/css/images/delete.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>';
+                }
+                result += '</ol>';
+            }
+
+            var div = document.getElementById('playlist');
+            if(div !== undefined) {
+                div.innerHTML = result;
+            }
+        })
+
+    }
+
+    pl.afterAddTrack = getUserFavorites;
+    pl.afterRemoveTrack = getUserFavorites;
+
     //usr.getInfo();
     getCollection()
     getUserFavorites();
-})
+});
