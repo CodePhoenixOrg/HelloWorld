1774d75637abb2b3a892021157eb3daac6ecb947
David Blanchard <dblanchard1@bbox.fr>
2017-05-14 17:05:17 +0200

    Move Phink client version into server tree

--- HelloWorld/vendor/phink/server/core.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var NestJS = NestJS || {}
-NestJS.Web = NestJS.Web || {}
-NestJS.Rest = NestJS.Rest || {}
-NestJS.Utils = NestJS.Utils || {}
-NestJS.Utils.IO = NestJS.Utils.IO || {}
-
